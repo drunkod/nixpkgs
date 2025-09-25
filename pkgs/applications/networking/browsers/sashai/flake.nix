62c34eb5407e01e4d3aa57de99341d8c45557e9d
Drunkod Library <you@example.com>
2025-09-25 18:11:12 +0500

    add flake.nix

---
+++ pkgs/applications/networking/browsers/sashai/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Build sashai-browser-1 from drunkod/nixpkgs fork";
+
+  inputs = {
+    # Pull nixpkgs from the fork/branch
+    nixpkgs.url = "github:drunkod/nixpkgs/sashai-browser-1";
+    # Replace "branch-name" with the actual branch you meant (e.g. "sashai-browser-1").
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.callPackage ./default.nix { };
+      # you can explicitly name it too:
+      packages.${system}.sashai-browser-1 = pkgs.callPackage ./default.nix { };
+    };
+}
